6d3ea1b5520ca4e51ff10b7f1de20af3032596f0
Nicolas <nicolas__du95@hotmail.fr>
2023-12-20 23:22:00 +0100

    Add "good" syntax tests, tests renamed

--- test/good/g3.c
+++
@@ -1,5 +0,0 @@
-int main(int argc, char argv)
-{
-    int a;
-    add(5 + 63 * 9, 2);
-}
---
+++ tmp/while2.c
@@ -0,0 +1,15 @@
+#define EOL ,'))'));
+#define SOL return 9,'(',(
+#define PARAMS int argc, char argv
+#define SOM int main(PARAMS) {
+#define EOM '}';}
+#define etoileetoile pow
+#define egal !=
+#define x *
+#define _ /
+#define o ,
+#define var __auto_type
+
+SOM
+    SOL etoileetoile((1 _ 2 x 3 + 4 - 5) egal (1 _ 2 x 3 + 4 - 5) o 2 EOL
+EOM
