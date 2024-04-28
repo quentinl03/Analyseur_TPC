b05fd1da73530a34bb2fb4030616466f5edaa9da
quentinl03 <q.laborde1220@gmail.com>
2024-04-28 18:41:55 +0200

    atexit on main function

--- src/main.c
+++ src/main.c
@@ -17,14 +17,34 @@
 #include "tree.h"
 #include "treeReader.h"
 
-int main(int argc, char* argv[]) {
-    extern FILE* yyin;
-    Node* abr = NULL;
+typedef struct {
+    ProgramSymbolTable symtable;
+    Node* abr;
     Option opt;
-    opt = parser(argc, argv);
+    FILE* file_out;
+} Program;
 
-    if (opt.path) {
-        yyin = fopen(opt.path, "r");
+Program PROGRAM;
+
+void atexit_function(void) {
+    if (PROGRAM.abr) {
+        deleteTree(PROGRAM.abr);
+    }
+    if (PROGRAM.file_out &&
+        PROGRAM.file_out != stdout &&
+        PROGRAM.file_out != stderr) {
+        fclose(PROGRAM.file_out);
+    }
+    //! @todo free symtable
+}
+
+int main(int argc, char* argv[]) {
+    atexit(atexit_function);
+    extern FILE* yyin;
+    PROGRAM.opt = parser(argc, argv);
+
+    if (PROGRAM.opt.path) {
+        yyin = fopen(PROGRAM.opt.path, "r");
         if (!yyin) {
             perror("fopen");
             fprintf(stderr, "End of execution.\n");
@@ -32,49 +52,42 @@ int main(int argc, char* argv[]) {
         }
     }
 
-    ErrorType err = parser_bison(yyin, &abr);
-    fclose(yyin);
+    ErrorType err = parser_bison(yyin, &PROGRAM.abr);
+    // fclose(yyin);
 
     if (IS_PARSE_ERROR(err)) {
         return EXIT_CODE(err);
     }
 
-    if (opt.flag_show_tree) {
-        printTree(abr);
+    if (PROGRAM.opt.flag_show_tree) {
+        printTree(PROGRAM.abr);
     }
 
-    if (opt.flag_only_tree) {
-        if (abr) {
-            deleteTree(abr);
-        }
+    if (PROGRAM.opt.flag_only_tree) {
         return EXIT_SUCCESS;
     }
 
     ProgramSymbolTable symtable;
-    err = ProgramSymbolTable_from_Prog(&symtable, abr);
-    if (opt.flag_symtabs) {
+    err = ProgramSymbolTable_from_Prog(&symtable, PROGRAM.abr);
+    if (PROGRAM.opt.flag_symtabs) {
         ProgramSymbolTable_print(&symtable);
     }
     if (IS_SEMANTIC(err) || IS_CRITICAL(err)) {
         return EXIT_CODE(err);
     }
 
-    err |= Semantic_check(abr, &symtable);
-    if (opt.flag_semantic || IS_SEMANTIC(err) || IS_CRITICAL(err)) {
+    err |= Semantic_check(PROGRAM.abr, &symtable);
+    if (PROGRAM.opt.flag_semantic || IS_SEMANTIC(err) || IS_CRITICAL(err)) {
         return EXIT_CODE(err);
     }
 
-    FILE* file = fopen(opt.output, "w");
+    FILE* file = fopen(PROGRAM.opt.output, "w");
     if (!file) {
         perror("fopen");
         return EXIT_CODE(ERR_FILE_OPEN);
     }
 
-    TreeReader_Prog(&symtable, abr, file);
-
-    if (abr) {
-        deleteTree(abr);
-    }
+    TreeReader_Prog(&symtable, PROGRAM.abr, file);
 
     return EXIT_SUCCESS;
 }
